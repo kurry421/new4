0744838a1aa7bdf71eced57a8961353886aeac39
kurry421 <kurry@Karrans-MBP.fios-router.home>
2016-10-14 19:57:17 -0400

    update

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require('../vendor/autoload.php');
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey("sk_test_3bgqDD5y1PkhCkUSWt2KibhB");
+\Stripe\Stripe::setApiKey("sk_test_kQ6aqDMPIjzADYwTnFFe0n65");
 
 // Get the credit card details submitted by the form
 $token = $_POST['stripeToken'];
