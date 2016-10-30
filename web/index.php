49c123113715a54473a8c0efccaf46d711120564
kurry421 <kurry@Karrans-MacBook-Pro.local>
2016-10-30 09:39:29 -0400

    try

--- web/index.php
+++ web/index.php
@@ -17,8 +17,36 @@ $userUid = $_POST['userUid'];
  
 if ($token == ""){
 	
-	echo "no token";
-	return;
+	try{
+  
+$subscription = \Stripe\Subscription::retrieve($customerStripePlanID);
+$subscription->plan = $newPlan;
+$subscription->save();
+
+  ));
+  
+     $response = array( 'status'=> 'Success', 'message'=>'Plan has been updated!', 'planEnd'=> $customerStripeID->subscriptions->data[0]->current_period_end);
+
+    // header("Content-Type: application/json");
+    echo json_encode($response);
+ // Use Stripe's library to make requests...
+} catch(\Stripe\Error\Card $e) {
+  // Since it's a decline, \Stripe\Error\Card will be caught
+  $body = $e->getJsonBody();
+  $err  = $body['error'];
+
+  print('Status is:' . $e->getHttpStatus() . "\n");
+  print('Type is:' . $err['type'] . "\n");
+  print('Code is:' . $err['code'] . "\n");
+  // param is '' in this case
+  print('Param is:' . $err['param'] . "\n");
+  print('Message is:' . $err['message'] . "\n");
+  //print('custoerId is:' . $err['customerId'] . "\n");
+
+  header("Content-Type: application/json");
+  echo json_encode($err);
+}
+  return;
 }
 
 //Create a Customer
